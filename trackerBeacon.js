ea56c5619481291a9af0b9f0aad2b1f532e0f5ae
sena <senagtms1@gmail.com>
2022-06-23 09:23:38 +0300

    Update trackerBeacon.js

--- trackerBeacon.js
+++ trackerBeacon.js
@@ -1,45 +1,117 @@
 const mysql = require("./app");
 const mqtt = require('mqtt');
 
-/**mqtt  */
+// mqtt filtreleme işlemleri
+function mqttVeri(dbTrackerListe,device,dbBeaconListe){
 
-function mqttCon(beaconKodu){
+var ortakdbmqtt;
+let data = []
+let ortakMac;
 
-const broker = 'mqtt://...';
+data=device.map((obj) => obj.address);   // databe ile mqtt mac adresleri uyuşuyor mu sorgusu
+ortakdbmqtt = dbTrackerListe.filter(function(val) { 
+    return data.indexOf(val) != -1;
+  });
+
+  if(ortakdbmqtt) {
+
+     ortakMac = device.reduce((dict, data) => { // id lere göre ayırma
+        if (!dict[data.address]) dict[data.address] = [];
+        dict[data.address].push(data);
+        return dict;
+      }, {});
+  
+
+      function sortObj(ortakMac){ // distance sıralama
+        return Object.entries(ortakMac) 
+          .reduce((acc, [key, value]) => 
+          (acc[key] = value.sort((a, b) => a.distance - b.distance), acc), {})
+      }
+      
+      var result = sortObj(ortakMac);
+      console.log(result);
+      return trakerİzleme(result,dbBeaconListe)
+
+    
+    }
+
+  else{
+        console.log("db de yok")
+    }
+console.log(data)
+    device= []; // diziyi boşalt
+}
+        
+   
+// veri tabanından beacon verileri listele
+
+function beaconListe(){
+    mysql.query("SELECT * FROM beacon ORDER BY metre ASC", (error, results, fields) => {
+        if (error) {
+             return console.error(error.message);
+          }
+          let dbBeaconListe = [];
+          let dbbeaconKodu= []
+          
+        results.forEach((r, i) => {
+      
+            dbBeaconListe[i] = [results[i].kodu, results[i].metre,results[i].koordinat,results[i].tipi,results[i].konumAlani]
+            dbbeaconKodu[i]=results[i].kodu;
+        });
+      return mqttCon(dbbeaconKodu,dbBeaconListe)
+      
+    })
+}
+
+
+
+/**mqtt Connection */
+
+function mqttCon(dbbeaconKodu,dbBeaconListe){
+
+const broker = 'mqtt://192.168.100.10:1883';
 
 const options = {
-    clientId: '',
+    clientId: 'nodeClient',
     username: '',
     password: ''
   }
-  let kod = []
-  let dbBeaconVerileri= []
-    if(beaconKodu == undefined){
+
+
+  let device = [];
+  let myTopic;
+
+
+    if(dbbeaconKodu == undefined){
+
         beaconListe()
-    }else{
-   
-    kod=beaconKodu;
-    console.log("Beacon Kodlar: ",kod)
-    dbBeaconVerileri = dbBeaconListe;
-    console.log("DB Beacon Verileri : " ,dbBeaconVerileri)
-   /**topic burada olacak */
+
     }
-
-    let myTopic =["scan/sena","scan/XXL02"];
-
+    else{
+   
+  
+      myTopic = "devices";
+      console.log("Beacon Kodlar: ",myTopic)
+  
     let client = mqtt.connect(broker, options);
     client.subscribe(myTopic)
 
 
     client.on('message', (myTopic, paylaod) => {
 
-        let device=JSON.parse(paylaod.toString())
-//         console.log(device.rssi);
-//         console.log(device.id.toString());
-//         console.log(device.distance);
+        device.push(JSON.parse(paylaod.toString()));
 
-  console.log(`topic`, myTopic)
- 
+        function push(props , device) { // topic device içine push edildi
+            let lastIndex = device.length - 1;
+            device[lastIndex] = Object.assign(props, device[lastIndex]);
+          }
+          push({
+              esp: dbbeaconKodu
+            }, device);
+
+     console.log(device.topic)
+     
+  // tracker verileri databaseden çekildi
   mysql.query("SELECT mac FROM tracker", (error, results, fields) => {
        
     if (error) {
@@ -50,50 +122,102 @@ const options = {
         dbTrackerListe[i] =results[i].mac;
     });
     // console.log(dbTrackerListe)
-    return mqttVeri(dbTrackerListe,device,myTopic);
+    return mqttVeri(dbTrackerListe,device,dbBeaconListe);
    
-})  })  }
+})  
+  }) 
+    }
+      }
 
-mqttCon()
+mqttCon() //
 
-function mqttVeri(dbTrackerListe,device,myTopic){
 
-    if(dbTrackerListe.find(dbVeri => dbVeri == device.id)){
+function trakerİzleme(ortakMac,dbBeaconListe){
 
-        let mqttVeriDizi = []
-        mqttVeriDizi.push(myTopic,device.rssi,device.distance,device.url, device.uuid)
+/** konum hesaplama- **/
+    var macDistance = Object.entries(ortakMac).reduce((acc, [key, value]) => 
+    (acc[key] = value.map((a) => a.distance), acc), {})
+    
+    let distanceTopla, konum;
+    size = 1;
+    let i = 0;
 
-        console.log("dizi indis sayısı: " + mqttVeriDizi.length)
+    for (var prop in macDistance) {
+    if (i < size) {
+    
+            distanceTopla= macDistance[prop][0] + macDistance[prop][1]
+            console.log("distance toplam: ",distanceTopla) 
 
-        console.log(device.id ,': ', mqttVeriDizi)
-
+            konum= ((20/distanceTopla) * macDistance[prop][0])+dbBeaconListe[0][1]
+            console.log("sonuc",konum) 
+          
+    }
+    else if(macDistance[prop].length == 1){
+        console.log("Tek distance: ", macDistance[prop][0]) 
+        console.log("***************************")
     }
     else{
-        console.log("db de yok")
+        console.log(console.log("tek elemanlı dizi bulunmamaktadır"))
     }
+    i++;
+   
+    }
+
+    /**tracker izleme verileri databaseden alındı */
+    let dbIzleme = [];
+    mysql.query("SELECT * FROM izlenme", (error, results, fields) => {
+       
+                if (error) {
+                     return console.error(error.message);
+                  }
+             
+                results.forEach((r, i) => {
+                    dbIzleme =[results[i].mac, results[i].sonKonum,results[i].sonKoordinat,results[i].sonMetre];
+                    return trakerİzlemedb(macDistance,konum,dbIzleme)
+                });
+            })
+       
 }
-function beaconListe(){
-    mysql.query("SELECT * FROM beacon", (error, results, fields) => {
-        if (error) {
-             return console.error(error.message);
-          }
-          let dbBeaconListe = [];
-          let beaconKodu= []
-        results.forEach((r, i) => {
-      
-            dbBeaconListe[i] = [results[i].kodu, results[i].metre,results[i].koordinat,results[i].tipi,results[i].konumAlani]     
-            beaconKodu[i]=results[i].kodu;
-        });
-    //   console.log(dbBeaconListe)
-    //   console.log(beaconKodu)
-      return mqttCon(beaconKodu,dbBeaconListe)
-      
-    })
+
+/* yeni tracker verileri işlemleri */
+function trakerİzlemedb(macDistance,konum,dbIzleme){
+   let ortakdizi = []
+   let ortakOlmayan= []
+            const mac = Object.keys(macDistance);
+
+            console.log()
+         
+var result = mac.reduce(function (prev, value) {
+
+  var isDuplicate = false;
+  for (var i = 0; i < mac.length; i++) {
+      if (value == dbIzleme[i]) {
+          isDuplicate = true;
+          mysql.query(`UPDATE izlenme SET sonKonum=?, sonKoordinat =?, sonMetre=? WHERE mac='${value}'`,["0","0",konum], (error, results, fields) => {
+                                if (error) {
+                                      return console.error(error.message);
+                                    }
+                                
+                                    // console.log(results);
+                                    console.log("update edildi")
+                                  });
+                        }
+          break;}
+
+  if (!isDuplicate) {
+    mysql.query(`INSERT INTO izlenme (mac,sonKonum,sonKoordinat,sonMetre) VALUES ('${value}','0','0','${konum}')`, function (error,results) {
+                  if (error) {
+                   return console.error(error.message);
+                  }
+              // console.log(results);
+              console.log("ekleme yapıldı")
+  }
+);}
+  return prev;
+
+}, []);
+
 }
 
 
-
-
-
-
-
+}
